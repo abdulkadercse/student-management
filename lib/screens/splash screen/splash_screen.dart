567a44ed6c79f6ca286023a60b49e2ae4a64570b
abdulkadercse <kaderhosen2002@gmail.com>
2022-12-03 15:24:52 +0600

    Student Management

---
+++ lib/screens/splash screen/splash_screen.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:firebase_counter_example/student_list.dart';
+import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import '../../authentication/sign_in.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  var currentUser = FirebaseAuth.instance.currentUser;
+  void checkUser() async{
+    await Future.delayed(const Duration(seconds: 2)).then((value) {
+      if(currentUser != null){
+        const StudentList().launch(context,isNewTask: true);
+      } else{
+        const SignIn().launch(context,isNewTask: true);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    checkUser();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(child: CircleAvatar(
+        backgroundColor: Colors.white,
+        radius: 70.0,
+        child: Text('Students',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
+      ),),
+    );
+  }
+}
